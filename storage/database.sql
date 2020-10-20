5ff760c7dc5c62ad8fe00ae9d3ce768fb3ce781d
Benjamín Cordero <ingbenjamincordero@gmail.com>
2020-10-20 09:23:18 -0400

    Set val of secuence

--- storage/database.sql
+++ storage/database.sql
@@ -1,2 +1,5 @@
-
-ALTER SEQUENCE tithes_id_seq RESTART WITH 77;
+SELECT SETVAL('types_id_seq', (SELECT MAX(id) + 1 FROM types));
+SELECT SETVAL('users_id_seq', (SELECT MAX(id) + 1 FROM users));
+SELECT SETVAL('transfers_id_seq', (SELECT MAX(id) + 1 FROM transfers));
+SELECT SETVAL('offerings_id_seq', (SELECT MAX(id) + 1 FROM offerings));
+SELECT SETVAL('tithes_id_seq', (SELECT MAX(id) + 1 FROM tithes));
