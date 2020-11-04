ff21d9efc3d72b1e7a211f7f8f1b5b0c962fa199
Benjamín Cordero <ingbenjamincordero@gmail.com>
2020-11-04 13:56:22 -0400

    Fix in Diezmos Complete inertia post

--- resources/js/Pages/Tithes/Index.vue
+++ resources/js/Pages/Tithes/Index.vue
@@ -241,7 +241,7 @@ export default {
         }, 250)
     },
     async complete() {
-        await this.$inertia.post(route("diezmos.complete", this.form));
+        await this.$inertia.post(route("diezmos.complete"), this.form);
 
         if (parseFloat(this.form.rate) > 0) {
             this.confirmingComplete = false;
